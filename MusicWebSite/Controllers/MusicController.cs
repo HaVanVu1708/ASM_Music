28e4cd7371e57422c8182ad28abdb2bdf6f3f820
ha van vu <62437220+HaVanVu1708@users.noreply.github.com>
2020-11-28 08:52:52 +0700

    ASM

---
+++ MusicWebSite/Controllers/MusicController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MusicWebSite.Models;
+using MusicWebSite.Services;
+
+namespace MusicWebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MusicController : Controller
+    {
+        public MusicController(JsonFileMusicServices musicServices)
+        {
+            this.MusicServices = musicServices;
+        }
+        public JsonFileMusicServices MusicServices { get; }
+       
+
+        [HttpGet]
+
+        public IEnumerable<Music> Get()
+        {
+            return MusicServices.GetMusics();
+        }
+
+        //HtppPath
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string MusicId,
+            [FromQuery] int Rating)
+        {
+            MusicServices.AddRating(MusicId, Rating);
+            return Ok();
+        }
+    }
+}
---
+++ MusicWebSite/Services/JsonFileMusicServices.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.Text.Json;
+using MusicWebSite.Models;
+
+namespace MusicWebSite.Services
+{
+    public class JsonFileMusicServices
+    {
+        public JsonFileMusicServices(IWebHostEnvironment webHostEnvironment)
+        {
+           WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "Music.json"); }
+        }
+
+        public IEnumerable<Music> GetMusics()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Music[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    }); ;
+            }
+        }
+
+        public void AddRating(string musicId, int rating)
+        {
+            var musics = GetMusics();
+
+            //LINQ
+
+            var query = musics.First(x => x.Id == musicId);
+
+            if(query.Ratings == null)
+            {
+                query.Ratings = new int[] { rating };
+            }
+            else
+            {
+                var ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Music>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    musics
+                    ); 
+            }
+        }
+    }
+}
